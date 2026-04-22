a39bfafc80c45dce470ee06ef7259d1700f96df0
AlexEstu15 <103544056+AlexEstu15@users.noreply.github.com>
2026-04-22 13:57:36 +0200

    Añadido el producto en model

---
+++ src/main/java/com/demo/model/Product.java
@@ -0,0 +1,22 @@
+package com.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer stock;
+
+    //TODO [Reverse Engineering] generate columns from DB
+}
