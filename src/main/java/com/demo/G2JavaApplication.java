9b745d33dda933c51a539f14f775f3ee561a7274
AlexEstu15 <103544056+AlexEstu15@users.noreply.github.com>
2026-04-22 12:15:28 +0200

    Mensaje o texto de prueba para probar los commits...

--- src/main/java/com/demo/G2JavaApplication.java
+++ src/main/java/com/demo/G2JavaApplication.java
@@ -9,5 +9,5 @@ public class G2JavaApplication {
     public static void main(String[] args) {
         SpringApplication.run(G2JavaApplication.class, args);
     }
-    //Hola se puede ver este comentario?
+    //Hola se puede ver este comentario
 }
