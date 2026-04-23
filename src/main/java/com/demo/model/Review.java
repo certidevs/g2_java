75f429075b5b78968b9e41798447d1d2a32d010f
Yomo <davivimix96@gmail.com>
2026-04-23 09:41:53 +0200

    Creado el ReviewRepository y añadido constru vacio y eggters y setter de Review

--- src/main/java/com/demo/model/Review.java
+++ src/main/java/com/demo/model/Review.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
-
+//test
 @Entity
 @Table(name="Reseñas")
 public class Review {
