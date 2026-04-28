ce31edae2044df799073a8d27fa02dfb5d800b31
AlexEstu15 <alexvegaban15@gmail.com>
2026-04-28 20:42:38 +0200

    #7 Añadido una lista para comprobar las conexiones con la tabla de datos

--- src/main/java/com/demo/controller/ProductController.java
+++ src/main/java/com/demo/controller/ProductController.java
@@ -1,11 +1,6 @@
 package com.demo.controller;
-
-import com.demo.model.Category;
 import com.demo.model.Product;
-import com.demo.repository.CategoryRepository;
 import com.demo.repository.ProductRepository;
-import com.demo.repository.PurchaseRepository;
-import com.demo.repository.ReviewRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,16 +13,11 @@ import java.util.List;
 
 public class ProductController {
     private final ProductRepository productRepository;
-    //private final ReviewRepository reviewRepository;
-    //private final PurchaseRepository purchaseRepository;
-    private final CategoryRepository categoryRepository;
 
     @GetMapping("/products")
-    public String productList(Model model){
+    public String productList( Model model){
         List<Product> products = productRepository.findAll();
-        List<Category> categories = categoryRepository.findAll();
         model.addAttribute("products", products);
-        model.addAttribute("categories", categories);
         return "products/productsList";
     }
 }
--- src/main/java/com/demo/repository/ProductRepository.java
+++ src/main/java/com/demo/repository/ProductRepository.java
@@ -12,4 +12,5 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     //Precio Mayor a menor
     List<Product> findProductsByPriceOrderByPriceDesc(Double price);
 
+
 }
