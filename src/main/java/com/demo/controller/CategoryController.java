e3fb6ab7cfa909d95c761ef27d06b79267b1ef38
diazguerraadrian-cmyk <diazguerraadrian@gmail.com>
2026-04-29 09:21:37 +0200

    Creado CategoryController y categoriesList

---
+++ src/main/java/com/demo/controller/CategoryController.java
@@ -0,0 +1,26 @@
+package com.demo.controller;
+
+import com.demo.model.Category;
+import com.demo.model.Product;
+import com.demo.repository.CategoryRepository;
+import com.demo.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class CategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+    @GetMapping("/category")
+    public String productList( Model model){
+        List<Category> categories = categoryRepository.findAll();
+        model.addAttribute("categories", categories);
+        return "categories/categoriesList";
+    }
+}
