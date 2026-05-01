d0b8e35538f6e8fe6923edfabe92ef0a875de100
AlexEstu15 <alexvegaban15@gmail.com>
2026-05-01 20:35:49 +0200

    #8 Mejoras de pagina y Añadido detalles-productos a la pagina web.

--- src/main/java/com/demo/controller/ProductController.java
+++ src/main/java/com/demo/controller/ProductController.java
@@ -7,8 +7,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
@@ -18,17 +20,37 @@ public class ProductController {
     private final ReviewRepository reviewRepository;
 
     @GetMapping("/products")
-    public String productList( Model model){
+    public String productList(Model model) {
         List<Product> products = productRepository.findAll();
         model.addAttribute("products", products);
         return "products/productsList";
     }
-    @GetMapping("/products-details")
-    public String productDetails(Model model){
-        List<Product> products = productRepository.findAll();
-        List<Review> reviews = reviewRepository.findAll();
-        model.addAttribute(products);
-        model.addAttribute(reviews);
-        return "products/productsList";
+
+    @GetMapping("products/{id}")
+    public String productDetail(@PathVariable Long id, Model model) {
+
+        Optional<Product> productOptional = productRepository.findById(id);
+        if (productOptional.isPresent()) {
+
+            Product product = productOptional.get();
+            model.addAttribute("product", product);
+            // opcional:
+            // cargar los platos (Dish) de este restaurant en el model
+            List<Review> reviews = reviewRepository.findByIdOrderByRatingAsc(id);
+            model.addAttribute("reviews", reviews);
+
+            // reviews
+            //List<Review> reviews = reviewRepository.findAll();
+            //List<Review> reviews = reviewRepository.findByRestaurant_IdOrderByCreationDateDesc(restaurant.getId());
+            //model.addAttribute("reviews", reviews); // accesibles desde HTML
+
+            return "products/products-details";
+
+        }
+
+        // El restaurante NO existe
+        // CUIDADO no apunta a HTML
+        // APUNTA al Controller
+        return "redirect:/productsList";
     }
 }
