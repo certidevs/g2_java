f96b5d4e02cecf57affd4ca3182697c951669ef9
AlexEstu15 <alexvegaban15@gmail.com>
2026-05-01 18:37:16 +0200

    #7 Añadido el get maping y sus tablas para cargar en html

--- src/main/java/com/demo/controller/ProductController.java
+++ src/main/java/com/demo/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.demo.controller;
 import com.demo.model.Product;
+import com.demo.model.Review;
 import com.demo.repository.ProductRepository;
+import com.demo.repository.ReviewRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,11 +15,20 @@ import java.util.List;
 
 public class ProductController {
     private final ProductRepository productRepository;
+    private final ReviewRepository reviewRepository;
 
     @GetMapping("/products")
     public String productList( Model model){
         List<Product> products = productRepository.findAll();
         model.addAttribute("products", products);
+        return "products/productsList";
+    }
+    @GetMapping("/products-details")
+    public String productDetails(Model model){
+        List<Product> products = productRepository.findAll();
+        List<Review> reviews = reviewRepository.findAll();
+        model.addAttribute(products);
+        model.addAttribute(reviews);
         return "products/productsList";
     }
 }
