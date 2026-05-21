7b4f3700b22d2e21ce16dde702276673352db85f
David <davivimix96@gmail.com>
2026-05-21 12:23:22 +0200

    Dependencias security en POM , Entidad User y enum Role creados

---
+++ src/main/java/com/demo/model/User.java
@@ -0,0 +1,39 @@
+package com.demo.model;
+
+import com.demo.model.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+public class User  implements UserDetails{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of( new SimpleGrantedAuthority(role.name()));
+    }
+}
